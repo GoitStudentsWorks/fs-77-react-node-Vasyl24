b67cbd104285caca7323f8e050eecf78d6f78862
GoitStudentsWorks <129747846+GoitStudentsWorks@users.noreply.github.com>
2023-10-21 10:56:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/mixmasters">
+        <BrowserRouter basename="/fs-77-react-node-Vasyl24">
         <Toaster position="top-right" reverseOrder={false} />
           <App />
         </BrowserRouter>
